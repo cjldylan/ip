777ac715b740c42ccf7da8bf25d48f81d6b84c0a
cjldylan <cjldylan@gmail.com>
2026-08-20 01:47:25 +0800

    added level1-echo

--- src/main/java/Baemax.java
+++ src/main/java/Baemax.java
@@ -1,6 +1,7 @@
 /**
  * The Baemax chatbot entry point.
  */
+import java.util.Scanner;
 public class Baemax {
     public static void main(String[] args) {
         String banner = "╔════════════════╗\n"
@@ -9,6 +10,17 @@ public class Baemax {
         System.out.println(banner);
 
         System.out.println("Hello, I am Baemax!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(command);
+        }
+        scanner.close();
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon.");
     }
